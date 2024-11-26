9f292f1f644813d5770eb4ec340c074e97c451b7
Victor! <vmarinpu@uoc.edu>
2024-11-26 21:55:31 +0100

    Adding a button detection

--- JustLanded/Assets/Code/PlayerController.cs
+++ JustLanded/Assets/Code/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,23 +8,29 @@ public class PlayerController : MonoBehaviour
 {
 
     public InputAction LeftAction;
+    private Rigidbody2D rigidbody;
 
 
     // Start is called before the first frame update
     void Start()
     {
         LeftAction.Enable();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("This is a log message");
+        Vector2 move = LeftAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        transform.position = position;
+
+        if (Gamepad.current.aButton.isPressed)
         {
-            Vector2 move = LeftAction.ReadValue<Vector2>();
-            Debug.Log(move);
-            Vector2 position = (Vector2)transform.position + move * 0.1f;
-            transform.position = position;
+            Debug.Log("The X button is pressed.");
+            rigidbody.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
         }
+
     }
 }
