57cef94f855bbf3819c9f48323104c66e098710a
Victor! <vmarinpu@uoc.edu>
2024-11-26 21:29:42 +0100

    Adding input system

--- JustLanded/Assets/Code/PlayerController.cs
+++ JustLanded/Assets/Code/PlayerController.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public InputAction LeftAction;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("This is a log message");
         
     }
 }
