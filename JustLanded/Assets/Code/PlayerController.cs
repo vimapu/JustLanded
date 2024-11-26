6fcbd6b09431954e32215deae1b641af06d6ca73
Victor! <vmarinpu@uoc.edu>
2024-11-26 21:39:19 +0100

    Adding distance to camera

--- JustLanded/Assets/Code/PlayerController.cs
+++ JustLanded/Assets/Code/PlayerController.cs
@@ -8,17 +8,22 @@ public class PlayerController : MonoBehaviour
 
     public InputAction LeftAction;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LeftAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("This is a log message");
-        
+        {
+            Vector2 move = LeftAction.ReadValue<Vector2>();
+            Debug.Log(move);
+            Vector2 position = (Vector2)transform.position + move * 0.1f;
+            transform.position = position;
+        }
     }
 }
