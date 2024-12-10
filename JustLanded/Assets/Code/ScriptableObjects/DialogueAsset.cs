e897ffd21a4dcdcaa8f83b277751bcf0b6e8ba18
Victor! <vmarinpu@uoc.edu>
2024-12-11 00:49:22 +0100

    Starting adding dialogue

---
+++ JustLanded/Assets/Code/ScriptableObjects/DialogueAsset.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DialogueAsset : ScriptableObject
+{
+
+    [TextArea]
+    public string[] dialogue;
+
+    
+
+}
