f9190f25ae28f21631b36b8b4f687c063036840e
Victor! <vmarinpu@uoc.edu>
2024-12-21 22:16:53 +0100

    Adding stats manager code

--- JustLanded/Assets/Code/Benson/StatsController.cs
+++ JustLanded/Assets/Code/Benson/StatsController.cs
@@ -9,9 +9,11 @@ public class StatsController : MonoBehaviour
     [SerializeField] float maxHealth;
 
     private float numOfGear = 0;
+    private float collectedGear = 0;
+    private float gearPoints = 0;
     private float numOfEnemies = 0;
     private float numOfKill = 0;
-    private float points = 0;
+    private float killPoints = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,15 +25,16 @@ public class StatsController : MonoBehaviour
         numOfGear += GameObject.FindGameObjectsWithTag("Gear").Length;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     // called by the observed when it dies
-    public void addDeadEnemy()
+    public void addDeadEnemy(float points)
     {
         numOfKill++;
+        killPoints += points;
+    }
+
+    public void addCollectedGear(float points)
+    {
+        collectedGear++;
+        gearPoints += points;
     }
 }
