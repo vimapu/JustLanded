47d15b0314d96f55e695ac2a73fc54d6737b42d1
Victor! <vmarinpu@uoc.edu>
2024-12-27 19:07:08 +0100

    Adding player controller to be killed

--- JustLanded/Assets/Code/Enemies/ShootingEnemyController.cs
+++ JustLanded/Assets/Code/Enemies/ShootingEnemyController.cs
@@ -63,7 +63,7 @@ public class ShootingEnemyController : MonoBehaviour, IKillable, Subject<DeadEne
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        var player = collider.GetComponent<DeathAndRespawnController>();
+        var player = collider.GetComponent<Player>();
         if (player != null)
         {
             player.Kill(this);
