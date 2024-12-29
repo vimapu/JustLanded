719611a57ee27b833bbb2df8c8fc14625b719d98
Victor! <vmarinpu@uoc.edu>
2024-12-29 23:32:39 +0100

    Adding script for fruits

---
+++ JustLanded/Assets/Code/Collectibles/CollectibleFruitController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleFruitController : MonoBehaviour, Subject<HealthItemCollectedEvent>
+{
+
+    [SerializeField] float value = 1f;
+
+    private List<IListener<HealthItemCollectedEvent>> Listeners;
+
+    void Awake()
+    {
+        Listeners = new List<IListener<HealthItemCollectedEvent>>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Notify(new HealthItemCollectedEvent(value));
+            Destroy(gameObject);
+        }
+    }
+
+
+
+    public void Add(IListener<HealthItemCollectedEvent> listener)
+    {
+        Debug.Log("Adding listener to gear controller");
+        Listeners.Add(listener);
+    }
+
+    public void Detach(IListener<HealthItemCollectedEvent> listener)
+    {
+        Listeners.Remove(listener);
+    }
+
+    public void Notify(HealthItemCollectedEvent notification)
+    {
+        foreach (IListener<HealthItemCollectedEvent> listener in Listeners)
+        {
+            listener.Notify(notification);
+        }
+    }
+}
--- JustLanded/Assets/Code/Events/HealthItemCollectedEvent.cs
+++ JustLanded/Assets/Code/Events/HealthItemCollectedEvent.cs
@@ -3,4 +3,9 @@
 public class HealthItemCollectedEvent
 {
     public float HealthPoints { get; }
+    public HealthItemCollectedEvent(float points)
+    {
+        HealthPoints = points;
+    }
+
 }
