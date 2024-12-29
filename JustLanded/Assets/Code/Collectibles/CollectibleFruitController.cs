dc4f231ab4b4419b1e1c7edbdf9661edf4e9a18a
Victor! <vmarinpu@uoc.edu>
2024-12-29 23:36:12 +0100

    Adding collectible potions

--- JustLanded/Assets/Code/Collectibles/CollectibleFruitController.cs
+++ JustLanded/Assets/Code/Collectibles/CollectibleFruitController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CollectibleFruitController : MonoBehaviour, Subject<HealthItemCollectedEvent>
 {
 
-    [SerializeField] float value = 1f;
+    [SerializeField] float value = 50f;
 
     private List<IListener<HealthItemCollectedEvent>> Listeners;
 
