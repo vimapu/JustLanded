accc3b228e25e412a062caddcc7d56fee9b20e6f
Victor! <vmarinpu@uoc.edu>
2024-12-31 23:37:55 +0100

    Adding audio source for springs

--- JustLanded/Assets/Code/Platforms/SpringController.cs
+++ JustLanded/Assets/Code/Platforms/SpringController.cs
@@ -6,10 +6,17 @@ public class SpringController : MonoBehaviour
 {
     [SerializeField] float jumpSpeed;
 
+    private AudioSource audioSource;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.CompareTag("Player"))
         {
+            audioSource.Play();
             var jumpingController = collider.gameObject.GetComponent<Player>();
             jumpingController.Jump(jumpSpeed);
         }
